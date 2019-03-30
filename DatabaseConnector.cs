b2a2770cb9b4e9fc82d4ef66c08f1d32da346f0d
gaffney1 <43519946+gaffney1@users.noreply.github.com>
2019-03-30 19:06:11 -0400

    Update DatabaseConnector.cs

--- DatabaseConnector.cs
+++ DatabaseConnector.cs
@@ -174,6 +174,133 @@ namespace FayeKeyILS
 
             return newid;
         }
+
+
+
+        //Start of materials
+
+        
+        private long generateMaterialID()
+        {
+            List<Material> allMaterial = new List<Material>();
+            List<long> allIDs = new List<long>();
+            long newid;
+            allMaterial = GetFullMaterialInfo();
+
+            foreach (Material mat in allMaterial)
+            {
+                allIDs.Add(mat.Id);
+            }
+            if (allIDs.Count > 0)
+            {
+                newid = allIDs.Max() + 1;
+            }
+            else
+            {
+                // If the database is empty, the ID will start at 1
+                newid = 1;
+            }
+            return newid;
+        }
+        public void addMaterial(long PLID, string mType, long mLL, string mName)
+        {
+            using (var db = new ILSDBEntities())
+            {//PatronLibraryID, MaterialType,MaterialLoanLength,
+                Material newMaterial = new Material
+                {
+                    patronLibraryID = PLID,
+                    materialType = mType,
+                    materialLoanLength = mLL,
+                    materialName = mName,
+                    Id = generateMaterialID()
+                };
+                db.Materials.Add(newMaterial);
+                db.SaveChanges();
+                db.Dispose();
+            }
+        }
+        public void removeMaterial(long pId)
+        {
+            using (var db = new ILSDBEntities())
+            {
+                Material r = db.Materials.First(i => i.Id == pId);
+                db.Materials.Attach(r);
+                db.Materials.Remove(r);
+                db.SaveChanges();
+                db.Dispose();
+            }
+
+        }
+        public List<Material> GetFullMaterialInfo()
+        {
+            List<long> id = new List<long>();// Material ID
+            List<long> patLID = new List<long>();// Patron Library ID
+            List<string> mType = new List<string>();//material type
+            List<long> mLL = new List<long>();//material loan length
+            List<string> mName = new List<string>();//material name
+
+            List<Material> material = new List<Material>();
+
+            id = GetMaterialID();
+            patLID = GetPatronID();
+            mType = GetMaterialType();
+            mLL = GetMaterialLoanLength();
+            mName = GetPatronPhone();
+
+            for (int i = 0; i < mName.Count(); i++)
+            {
+                material.Add(new Material { Id = id[i], patronLibraryID = patLID[i], materialType = mType[i], materialLoanLength = mLL[i], materialName = mName[i] });
+            }
+
+            return material;
+        }
+
+        public void updateMaterial(long PLID, string mType, long mLL, string mName)
+        {
+            using (var db = new ILSDBEntities())
+            {
+                Material m = db.Materials.First(i => i.Id == PLID);
+                m.patronLibraryID = PLID;
+                m.materialType = mType;
+                m.materialLoanLength = mLL;
+                m.materialName = mName;
+                db.SaveChanges();
+                db.Dispose();
+            }
+        }
+        public List<long> GetMaterialID()
+        {
+            List<long> materialIDs = new List<long>();
+
+            using (var db = new ILSDBEntities())
+            {
+                materialIDs = (from mats in db.Materials select mats.Id).ToList();
+            }
+
+            return materialIDs;
+        }
+        public List<string> GetMaterialType()
+        {
+            List<string> materialType = new List<string>();
+
+            using (var db = new ILSDBEntities())
+            {
+                materialType = (from mats in db.Materials select mats.materialType).ToList();
+            }
+
+            return materialType;
+        }
+        public List<string> GetMaterialName()
+        {
+            List<string> materialName = new List<string>();
+
+            using (var db = new ILSDBEntities())
+            {
+                materialName = (from mats in db.Materials select mats.materialName).ToList();
+            }
+
+            return materialName;
+        }
     }
 }
 
